263d575c638c7f4a06e41148697b34797ec00ff5
Job <hj93@protonmail.com>
2021-03-02 11:19:32 -0800

    added regularization.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,17 @@ from model.net import Net
 from model.data_loader import(
     instantiate_data,
     instantiate_training_data,
+    instantiate_val_data,
     )
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from train import training_loop
+from evaluate import(
+    evaluate,
+    evaluate_validation,
+    evaluate_training,
+    )
 
 def main():
 
@@ -15,13 +21,18 @@ def main():
     data_path = "../Mnist/"
 
     mnist = instantiate_training_data(data_path)
+    mnist_val = instantiate_val_data(data_path)
     
     train_loader = torch.utils.data.DataLoader(mnist, batch_size=64)
+    val_loader = torch.utils.data.DataLoader(mnist_val, batch_size=64)
     
     optimizer = optim.SGD(model.parameters(), lr=1e-2)
     
     loss_fn = nn.CrossEntropyLoss()
 
+    training_string = "Training"
+    val_string = "Val"
+    
     training_loop(
         n_epochs = 100,
         optimizer = optimizer,
@@ -29,6 +40,9 @@ def main():
         loss_fn = loss_fn,
         train_loader = train_loader,
         )
-
+    
+    evaluate_training(model, train_loader, training_string)
+    evaluate_validation(model, val_loader, val_string)
+    
 main()
 
--- src/model/net.py
+++ src/model/net.py
@@ -10,16 +10,20 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(n_channels, 64, (3,3))
         self.act1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d((2,2), stride=(2,2))
+        self.dropout = nn.Dropout(0.2)
         # VGG block 2
         self.conv2 = nn.Conv2d(64, 64, (3,3))
         self.act2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d((2,2), stride=(2,2))
+        self.dropout2 = nn.Dropout(0.2)
         # VGG block 3
         self.conv3 = nn.Conv2d(64, 128, (3,3))
         self.act3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d((2,2), stride=(2,2))
+        self.dropout3 = nn.Dropout(0.2)
         # Fully connected layer
         self.f1 = nn.Linear(128 * 1 * 1, 1000)
+        self.dropout4 = nn.Dropout(0.5)
         self.act4 = nn.ReLU()
         # Output layer
         self.f2 = nn.Linear(1000, 10)
@@ -31,19 +35,23 @@ class Net(nn.Module):
         X = self.conv1(X)
         X = self.act1(X)
         X = self.pool1(X)
+        X = self.dropout(X)
         # VGG block 2
         X = self.conv2(X)
         X = self.act2(X)
         X = self.pool2(X)
+        X = self.dropout2(X)
         # VGG block 3
         X = self.conv3(X)
         X = self.act3(X)
         X = self.pool3(X)
+        X = self.dropout3(X)
         # Flatten
         X = X.view(-1, 128)
         # Fully connected layer
         X = self.f1(X)
         X = self.act4(X)
+        X = self.dropout4(X)
         # Output layer
         X = self.f2(X)
         X = self.act5(X)
